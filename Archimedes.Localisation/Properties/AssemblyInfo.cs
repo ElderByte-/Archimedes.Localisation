4fefde83ccf5003cf047f8b178cd41b89b489435
IsNull <pascalbuettiker@bluewin.ch>
2015-09-30 17:49:23 +0200

    deployed to nuget

--- Archimedes.Localisation/Properties/AssemblyInfo.cs
+++ Archimedes.Localisation/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Archimedes.Localisation")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides a localisationa / translation solution which is more flexible than the .NET satelite assemblies.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ElderByte")]
 [assembly: AssemblyProduct("Archimedes.Localisation")]
