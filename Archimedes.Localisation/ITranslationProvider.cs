1f370f5b7b71a6f982dd8fa690c6fe11c45b8f94
IsNull <pascalbuettiker@bluewin.ch>
2015-09-30 16:48:25 +0200

    initial commit of library code

---
+++ Archimedes.Localisation/ITranslationProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archimedes.Localisation
+{
+    public interface ITranslationProvider
+    {
+        string GetTranslation(string key);
+
+        string GetTranslation(string key, object[] args);
+    }
+}
---
+++ Archimedes.Localisation/Translator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archimedes.Localisation
+{
+    public static class Translator
+    {
+        private static ITranslationProvider _translationProvider;
+
+        public static ITranslationProvider TranslationProvider
+        {
+            get { return _translationProvider; }
+            set { _translationProvider = value; }
+        }
+
+
+        public static string GetTranslation(string key)
+        {
+            if (TranslationProvider != null)
+            {
+                return TranslationProvider.GetTranslation(key);
+            }
+            return "[" + key + "]";
+        }
+
+        public static string GetTranslation(string key, params object[] args)
+        {
+            if (TranslationProvider != null)
+            {
+                return TranslationProvider.GetTranslation(key, args);
+            }
+            return "[" + key + "]";
+        }
+    }
+}
